0bbae0203b0702e6417ad2394a9a6be070821f18
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2022-05-23 12:11:35 +0000

    Creating new service pension-scheme-event-reporting-stubs

---
+++ app/uk/gov/hmrc/pensionschemeeventreportingstubs/config/AppConfig.scala
@@ -0,0 +1,10 @@
+package uk.gov.hmrc.pensionschemeeventreportingstubs.config
+
+import javax.inject.{Inject, Singleton}
+import play.api.Configuration
+
+@Singleton
+class AppConfig @Inject()(config: Configuration) {
+
+  val appName: String = config.get[String]("appName")
+}
