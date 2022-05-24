aee43cb3b0c6cff4e5d7578d20342a5bb1b6beb3
jigneshkumarpatel <86969870+jigneshkumarpatel@users.noreply.github.com>
2022-05-24 17:29:10 +0100

    PODS-7026:fix error

--- app/controllers/EventReportController.scala
+++ app/controllers/EventReportController.scala
@@ -1,23 +1,31 @@
 /*
  * Copyright 2022 HM Revenue & Customs
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package controllers
 
 import com.google.inject.Inject
-import config.AppConfig
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
-import schemaValidator.SchemaValidator
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
-import utils.{APIResponses, JsonUtils}
+import utils.APIResponses
 
+import javax.inject.Singleton
 import scala.concurrent.Future
 
+@Singleton()
 class EventReportController @Inject()(
-                                       schemaValidator: SchemaValidator,
-                                       appConfig: AppConfig,
-                                       jsonUtils: JsonUtils,
                                        cc: ControllerComponents
                                      ) extends BackendController(cc) with APIResponses {
 
