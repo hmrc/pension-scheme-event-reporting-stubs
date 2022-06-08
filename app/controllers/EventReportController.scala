1fc5fbf4c7861f885e4c886e6f8943c86c5c592c
Mary1-5 <marymansha15@gmail.com>
2022-06-08 15:51:46 +0100

    WIP

--- app/controllers/EventReportController.scala
+++ app/controllers/EventReportController.scala
@@ -17,12 +17,9 @@
 package controllers
 
 import com.google.inject.Inject
-import play.api.mvc.{Action, AnyContent, ControllerComponents}
-import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
-import utils.APIResponses
 
+import java.time.LocalDate
 import javax.inject.Singleton
-import scala.concurrent.Future
 
 @Singleton()
 class EventReportController @Inject()(
@@ -35,6 +32,87 @@ class EventReportController @Inject()(
         case Some(_) => Future.successful(Ok(createCompileEventReportSummarySuccessResponse))
         case _ => Future.successful(BadRequest(invalidPayload))
       }
+  }
+
+  def getEROverview(pstr: String, startDate: String, endDate: String): Action[AnyContent] = Action.async {
+
+    def defaultOverview = {
+      Json.arr(
+        Json.obj(
+          "periodStartDate" -> startDate,
+          "periodEndDate" -> endDate,
+          "numberOfVersions" -> 1,
+          "submittedVersionAvailable" -> "No",
+          "compiledVersionAvailable" -> "Yes"
+        )
+      )
+    }
+
+    val path = "conf/resources/data/getOverview"
+    val notFoundPSTR = Seq("24000001IN", "24000007IN", "24000006IN", "24000002IN", "00000042IN")
+    val erPerfTestPstrPattern: String = """^34000[0-9]{3}IN$"""
+
+    if (notFoundPSTR.contains(pstr) || pstr.matches(erPerfTestPstrPattern))
+      Future.successful(NotFound(NotFoundResponse))
+    else {
+      val jsValue = jsonUtils.readJsonIfFileFound(s"$path/$pstr.json")
+        .getOrElse(defaultOverview)
+
+      Future.successful(Ok(filterOverview(jsValue, startDate, endDate)))
+    }
+  }
+
+  def getER20AOverview(pstr: String, startDate: String, endDate: String): Action[AnyContent] = Action.async {
+
+    def defaultOverview = {
+      Json.arr(
+        Json.obj(
+          "periodStartDate" -> startDate,
+          "periodEndDate" -> endDate,
+          "numberOfVersions" -> 1,
+          "submittedVersionAvailable" -> "No",
+          "compiledVersionAvailable" -> "Yes"
+        )
+      )
+    }
+
+    val path = "conf/resources/data/getOverview"
+    val notFoundPSTR = Seq("24000001IN", "24000007IN", "24000006IN", "24000002IN", "00000042IN")
+    val erPerfTestPstrPattern: String = """^34000[0-9]{3}IN$"""
+
+    if (notFoundPSTR.contains(pstr) || pstr.matches(erPerfTestPstrPattern))
+      Future.successful(NotFound(NotFoundResponse))
+    else {
+      val jsValue = jsonUtils.readJsonIfFileFound(s"$path/$pstr.json")
+        .getOrElse(defaultOverview)
+
+      Future.successful(Ok(filterOverview(jsValue, startDate, endDate)))
+    }
+  }
+
+  private case class Overview(
+                               periodStartDate: LocalDate,
+                               periodEndDate: LocalDate,
+                               tpssReportPresent: Option[String],
+                               numberOfVersions: Option[Int],
+                               submittedVersionAvailable: Option[String],
+                               compiledVersionAvailable: Option[String]
+                             )
+
+  private def filterOverview(jsValue: JsValue, startDate: String, endDate: String): JsValue = {
+    implicit val formats: Format[Overview] = Json.format[Overview]
+    jsValue.validate[Seq[Overview]] match {
+      case JsSuccess(seqOverview, _) =>
+        val compareStartDate = LocalDate.parse(startDate)
+        val compareEndDate = LocalDate.parse(endDate)
+
+        val filteredSeqOverview = seqOverview.filter(o =>
+          (o.periodStartDate.isAfter(compareStartDate) || o.periodStartDate.isEqual(compareStartDate)) &&
+            (o.periodEndDate.isBefore(compareEndDate) || o.periodEndDate.isEqual(compareEndDate))
+        )
+        Json.toJson(filteredSeqOverview)
+      case JsError(_) => throw new RuntimeException("Unable to read json")
+    }
   }
 }
 
