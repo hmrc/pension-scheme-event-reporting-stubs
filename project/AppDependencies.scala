e89b92f4d0d62126c1481da13d6c6ed7ad996076
grant.robson <grant.robson@digital.hmrc.gov.uk>
2023-02-14 10:46:22 +0000

    Correct library

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % playVersion           % "test,it",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"  % playVersion           % "test,it",
     "org.scalatest"           %% "scalatest"                  % "3.2.15"              % "test,it",
     "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0"               % "test",
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.64.0"              % "test, it"
