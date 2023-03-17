0b7c20393269e8a196077dfe036c4a4a110e8080
Mary1-5 <marymansha15@gmail.com>
2023-03-17 12:12:02 +0000

    PODS-8005 adding CSV files for event 6 and event 23 Bulk data

--- app/utils/StubDataGenerator.scala
+++ app/utils/StubDataGenerator.scala
@@ -29,6 +29,55 @@ object StubDataGenerator extends App {
     pw.close()
   }
 
+  private val protectionTypes = Seq("enhancedLifetimeAllowance", "enhancedProtection", "fixedProtection",
+    "fixedProtection2014", "fixedProtection2016", "individualProtection2014", "individualProtection2016"
+  )
+  private val protectionTypesCsv = Seq("Enhanced lifetime allowance", "Enhanced protection", "Fixed protection",
+    "Fixed protection 2014", "Fixed protection 2016", "Individual protection 2014", "Individual protection 2016"
+  )
+
+  def generateEvent6SummaryCsv( numberOfMembers: Int, year: Int): String = {
+    val headers = "First name," +
+      "Last name," +
+      "National Insurance number," +
+      "Type of protection held for the crystallisation (see LTA certificate)," +
+      "Protection type reference," +
+      "Total amount crystallised (£)," +
+      "Date the benefit was crystallised (XX/XX/XXXX)\n"
+    val arrayOfMembers = for {
+      _ <- (1 to numberOfMembers).toList
+    } yield {
+      s"${Name.first_name}," +
+        s"${Name.last_name}," +
+        s"AB${new Random().between(100000, 999999)}C," +
+        s"${new Random().shuffle(protectionTypesCsv).head}," +
+        s"${new Random().between(10000000, 99999999)}," +
+        s"${BigDecimal(new Random().between(1: Float, 1000: Float)).setScale(2, BigDecimal.RoundingMode.HALF_UP)}," +
+        s"${year}-11-0${new Random().between(1, 9)}\n"
+    }
+    val membersListAsString = arrayOfMembers.mkString
+    headers + membersListAsString
+  }
+
+  def generateEvent22Or23SummaryCsv(numberOfMembers: Int, year: Int): String = {
+    val headers = "First name," +
+      "Last name," +
+      "National Insurance number," +
+      "In which tax year was the money purchase pension savings statement issued? (XXXX to XXXX)," +
+      "What is the total of the member’s pension input amounts for money purchase arrangements under the scheme for the tax year that the purchase pension savings statement was issued? (£)\n"
+    val arrayOfMembers = for {
+      _ <- (1 to numberOfMembers).toList
+    } yield {
+      s"${Name.first_name}," +
+        s"${Name.last_name}," +
+        s"AB${new Random().between(100000, 999999)}C," +
+        s"$year to ${year + 1}," +
+        s"${BigDecimal(new Random().between(1: Float, 1000: Float)).setScale(2, BigDecimal.RoundingMode.HALF_UP)}\n"
+    }
+    val membersListAsString = arrayOfMembers.mkString
+    headers + membersListAsString
+  }
+
   def generateEvent22Or23SummaryJson(eventType: String, numberOfMembers: Int, year: Int): JsObject = {
     val arrayOfMembers = for {
       _ <- (1 to numberOfMembers).toList
@@ -58,9 +107,6 @@ object StubDataGenerator extends App {
   }
 
   def generateEvent6SummaryJson(eventType: String, numberOfMembers: Int, year: Int): JsObject = {
-    val protectionTypes = Seq("enhancedLifetimeAllowance", "enhancedProtection", "fixedProtection",
-      "fixedProtection2014", "fixedProtection2016", "individualProtection2014", "individualProtection2016"
-    )
     val arrayOfMembers = for {
       _ <- (1 to numberOfMembers).toList
     } yield {
@@ -70,12 +116,11 @@ object StubDataGenerator extends App {
           "lastName" -> Name.last_name,
           "nino" -> s"AB${new Random().between(100000, 999999)}C"
         ),
-        //TODO: randomise value of typeOfProtection options
         "typeOfProtection" -> s"${new Random().shuffle(protectionTypes).head}",
         "inputProtectionType" ->  s"${new Random().between(10000000, 99999999)}",
         "AmountCrystallisedAndDate" -> Json.obj(
           "amountCrystallised" -> BigDecimal(new Random().between(1: Float, 1000: Float)).setScale(2, BigDecimal.RoundingMode.HALF_UP),
-          "crystallisedDate" -> s"${year}-11-0${new Random().between(1, 9)}",
+          "crystallisedDate" -> s"$year-11-0${new Random().between(1, 9)}",
         )
       )
     }
@@ -94,15 +139,22 @@ object StubDataGenerator extends App {
   }
 
   // Uncomment below to write files.
-  private val numOfMembers = 100
-   private val taxYear = DateHelper.currentYear - 1
+  private val numOfMembers = 18000
+  private val taxYear = DateHelper.currentYear - 1
 
   // generate data for event22
   //writeFileToConfResources("event22", generateEvent22Or23SummaryJson("event22", numOfMembers, taxYear).toString(), s"${numOfMembers.toString}Members${taxYear}Payload.json")
 
   //generate data for event23
-//  writeFileToConfResources("event23", generateEvent22Or23SummaryJson("event23", numOfMembers, taxYear).toString(), s"${numOfMembers.toString}Members${taxYear}Payload.json")
+  //writeFileToConfResources("event23", generateEvent22Or23SummaryJson("event23", numOfMembers, taxYear).toString(), s"${numOfMembers.toString}Members${taxYear}Payload.json")
 
-  // generate data for event6
-  writeFileToConfResources("event6", generateEvent6SummaryJson("event6", numOfMembers, taxYear).toString(), s"${numOfMembers.toString}Members${taxYear}Payload.json")
+  // generate json data for event6
+  //writeFileToConfResources("event6", generateEvent6SummaryJson("event6", numOfMembers, taxYear).toString(), s"${numOfMembers.toString}Members${taxYear}Payload.json")
+
+  // generate csv data for event6
+  //writeFileToConfResources("event6", generateEvent6SummaryCsv(numOfMembers, taxYear), s"${numOfMembers.toString}Members${taxYear}Payload.csv")
+
+  // generate csv data for event23
+  writeFileToConfResources("event23", generateEvent22Or23SummaryCsv(numOfMembers, taxYear), s"${numOfMembers.toString}Members${taxYear}Payload.csv")
+
 }
