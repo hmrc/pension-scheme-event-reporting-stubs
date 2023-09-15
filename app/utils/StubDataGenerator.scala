9902172b4bbbe1f2f83f15d6a53e50504e03eb7e
ngmjohnstone <ngmjohnstone@yahoo.com>
2023-09-15 17:12:23 +0100

    PODS-new-stub-data: Added new gens + JSON data
    Code:
    - Generators and JSON data added for Events 2, 3, 4, 5, 7, 8, 8A

--- app/utils/StubDataGenerator.scala
+++ app/utils/StubDataGenerator.scala
@@ -24,12 +24,15 @@ import faker.Name
 object StubDataGenerator extends App {
   def writeFileToConfResources(eventType: String, content:String, fileName:String):Unit = {
     import java.io._
-    val pw = new PrintWriter(new File( s"conf/resources/data/api1832/${eventType}PaginationTestPayloads/$fileName" ))
+    // Original:
+    // val pw = new PrintWriter(new File( s"conf/resources/data/api1832/${eventType}PaginationTestPayloads/$fileName" ))
+    val pw = new PrintWriter(new File( s"conf/resources/data/api1832/$fileName" ))
     pw.write(content)
     pw.close()
   }
 
-  def generateEvent6SummaryCsv( numberOfMembers: Int, year: Int): String = {
+
+  def generateEvent6SummaryCsv(numberOfMembers: Int, year: Int): String = {
 
     val protectionTypesCsv = Seq("Enhanced lifetime allowance", "Enhanced protection", "Fixed protection",
       "Fixed protection 2014", "Fixed protection 2016", "Individual protection 2014", "Individual protection 2016"
@@ -43,11 +46,11 @@ object StubDataGenerator extends App {
       "Total amount crystallised (£)," +
       "Date the benefit was crystallised (XX/XX/XXXX)\n"
     val arrayOfMembers = for {
-      _ <- (1 to numberOfMembers).toList
+      num <- (1 to numberOfMembers).toList
     } yield {
       s"${Name.first_name}," +
         s"${Name.last_name}," +
-        s"AB${new Random().between(100000, 999999)}C," +
+        s"AB${100000 + num}C," +
         s"${new Random().shuffle(protectionTypesCsv).head}," +
         s"${new Random().between(10000000, 99999999)}," +
         s"${BigDecimal(new Random().between(1: Float, 1000: Float)).setScale(2, BigDecimal.RoundingMode.HALF_UP)}," +
@@ -64,11 +67,11 @@ object StubDataGenerator extends App {
       "In which tax year was the money purchase pension savings statement issued? (XXXX to XXXX)," +
       "What is the total of the member’s pension input amounts for money purchase arrangements under the scheme for the tax year that the purchase pension savings statement was issued? (£)\n"
     val arrayOfMembers = for {
-      _ <- (1 to numberOfMembers).toList
+      num <- (1 to numberOfMembers).toList
     } yield {
       s"${Name.first_name}," +
         s"${Name.last_name}," +
-        s"AB${new Random().between(100000, 999999)}C," +
+        s"AB${100000 + num}C," +
         s"$year to ${year + 1}," +
         s"${BigDecimal(new Random().between(1: Float, 1000: Float)).setScale(2, BigDecimal.RoundingMode.HALF_UP)}\n"
     }
@@ -83,11 +86,11 @@ object StubDataGenerator extends App {
       "In which tax year was the annual allowance exceeded? (XXXX to XXXX)," +
       "What is the total of the member's pension input amounts for all arrangements under the scheme in the tax year that the annual allowance was exceeded? (£)\n"
     val arrayOfMembers = for {
-      _ <- (1 to numberOfMembers).toList
+      num <- (1 to numberOfMembers).toList
     } yield {
       s"${Name.first_name}," +
         s"${Name.last_name}," +
-        s"AB${new Random().between(100000, 999999)}C," +
+        s"AB${100000 + num}C," +
         s"$year to ${year + 1}," +
         s"${BigDecimal(new Random().between(1: Float, 1000: Float)).setScale(2, BigDecimal.RoundingMode.HALF_UP)}\n"
     }
@@ -95,15 +98,260 @@ object StubDataGenerator extends App {
     headers + membersListAsString
   }
 
+
+  def generateEvent2SummaryJson(eventType: String, numberOfMembers: Int): JsObject = {
+    val arrayOfMembers = for {
+      num <- (1 to numberOfMembers).toList
+    } yield {
+      Json.parse(
+        s"""
+          |{
+          |  "deceasedMembersDetails": {
+          |    "firstName": "Hello",
+          |    "lastName": "World",
+          |    "nino": "AB${100000 + num}C"
+          |  },
+          |  "beneficiaryDetails": {
+          |    "firstName": "Bonjour",
+          |    "lastName": "Monde",
+          |    "nino": "AB${200000 + num}C"
+          |  },
+          |  "amountPaid": 10.01,
+          |  "datePaid": "2022-12-12"
+          |}
+          |""".stripMargin)
+    }
+    Json.obj(
+      "_id" -> "Change this entire line to match the original Mongo document",
+      "apiTypes" -> "1830",
+      "pstr" -> "Change this entire line to match the original Mongo document",
+      "data" -> Json.obj(
+        eventType -> Json.obj(
+          "members" -> arrayOfMembers
+        )
+      ),
+      "expireAt" -> "Change this entire line to match the original Mongo document",
+      "lastUpdated" -> "Change this entire line to match the original Mongo document"
+    )
+  }
+
+  def generateEvent3SummaryJson(eventType: String, numberOfMembers: Int): JsObject = {
+    val arrayOfMembers = for {
+      num <- (1 to numberOfMembers).toList
+    } yield {
+      Json.parse(
+        s"""
+          |{
+          |  "membersDetails": {
+          |    "firstName": "Hello",
+          |    "lastName": "World",
+          |    "nino": "AB${100000 + num}C"
+          |  },
+          |  "benefitType": {
+          |    "reasonBenefitTaken": "other",
+          |    "freeText": "Reason"
+          |  },
+          |  "paymentDetails": {
+          |    "amountPaid": 10.01,
+          |    "eventDate": "2022-12-12"
+          |  }
+          |}
+          |""".stripMargin)
+    }
+    Json.obj(
+      "_id" -> "Change this entire line to match the original Mongo document",
+      "apiTypes" -> "1830",
+      "pstr" -> "Change this entire line to match the original Mongo document",
+      "data" -> Json.obj(
+        eventType -> Json.obj(
+          "members" -> arrayOfMembers
+        )
+      ),
+      "expireAt" -> "Change this entire line to match the original Mongo document",
+      "lastUpdated" -> "Change this entire line to match the original Mongo document"
+    )
+  }
+
+  def generateEvent4SummaryJson(eventType: String, numberOfMembers: Int): JsObject = {
+    val arrayOfMembers = for {
+      num <- (1 to numberOfMembers).toList
+    } yield {
+      Json.parse(
+  s"""
+    |{
+    |  "membersDetails": {
+    |    "firstName": "Hello",
+    |    "lastName": "World",
+    |    "nino": "AB${100000 + num}C"
+    |  },
+    |  "paymentDetails": {
+    |    "amountPaid": 10.01,
+    |    "eventDate": "2022-12-12"
+    |  }
+    |}
+    |""".stripMargin)
+    }
+    Json.obj(
+      "_id" -> "Change this entire line to match the original Mongo document",
+      "apiTypes" -> "1830",
+      "pstr" -> "Change this entire line to match the original Mongo document",
+      "data" -> Json.obj(
+        eventType -> Json.obj(
+          "members" -> arrayOfMembers
+        )
+      ),
+      "expireAt" -> "Change this entire line to match the original Mongo document",
+      "lastUpdated" -> "Change this entire line to match the original Mongo document"
+    )
+  }
+
+  def generateEvent5SummaryJson(eventType: String, numberOfMembers: Int): JsObject = {
+    val arrayOfMembers = for {
+      num <- (1 to numberOfMembers).toList
+    } yield {
+      Json.parse(
+        s"""
+          |  {
+          |  "membersDetails": {
+          |    "firstName": "Hello",
+          |    "lastName": "World",
+          |    "nino": "AB${100000 + num}C"
+          |  },
+          |  "paymentDetails": {
+          |    "amountPaid": 10.01,
+          |    "eventDate": "2022-12-12"
+          |  }
+          |}
+          |""".stripMargin)
+    }
+    Json.obj(
+      "_id" -> "Change this entire line to match the original Mongo document",
+      "apiTypes" -> "1830",
+      "pstr" -> "Change this entire line to match the original Mongo document",
+      "data" -> Json.obj(
+        eventType -> Json.obj(
+          "members" -> arrayOfMembers
+        )
+      ),
+      "expireAt" -> "Change this entire line to match the original Mongo document",
+      "lastUpdated" -> "Change this entire line to match the original Mongo document"
+    )
+  }
+
+  def generateEvent7SummaryJson(eventType: String, numberOfMembers: Int): JsObject = {
+    val arrayOfMembers = for {
+      num <- (1 to numberOfMembers).toList
+    } yield {
+      Json.parse(
+        s"""
+          |{
+          |  "membersDetails": {
+          |    "firstName": "Hello",
+          |    "lastName": "World",
+          |    "nino": "AB${100000 + num}C"
+          |  },
+          |  "lumpSumAmount": 10.01,
+          |  "crystallisedAmount": 10.01,
+          |  "paymentDate": {
+          |    "date": "2022-12-12"
+          |  }
+          |}
+          |""".stripMargin)
+    }
+    Json.obj(
+      "_id" -> "Change this entire line to match the original Mongo document",
+      "apiTypes" -> "1830",
+      "pstr" -> "Change this entire line to match the original Mongo document",
+      "data" -> Json.obj(
+        eventType -> Json.obj(
+          "members" -> arrayOfMembers
+        )
+      ),
+      "expireAt" -> "Change this entire line to match the original Mongo document",
+      "lastUpdated" -> "Change this entire line to match the original Mongo document"
+    )
+  }
+
+  def generateEvent8SummaryJson(eventType: String, numberOfMembers: Int): JsObject = {
+    val arrayOfMembers = for {
+      num <- (1 to numberOfMembers).toList
+    } yield {
+      Json.parse(
+        s"""
+          |  {
+          |  "membersDetails": {
+          |    "firstName": "Hello",
+          |    "lastName": "World",
+          |    "nino": "AB${100000 + num}C"
+          |  },
+          |  "typeOfProtection": "primaryProtection",
+          |  "typeOfProtectionReference": "12345678",
+          |  "lumpSumAmountAndDate": {
+          |    "lumpSumAmount": 10.01,
+          |    "lumpSumDate": "2022-12-12"
+          |  }
+          |}
+          |""".stripMargin)
+    }
+    Json.obj(
+      "_id" -> "Change this entire line to match the original Mongo document",
+      "apiTypes" -> "1830",
+      "pstr" -> "Change this entire line to match the original Mongo document",
+      "data" -> Json.obj(
+        eventType -> Json.obj(
+          "members" -> arrayOfMembers
+        )
+      ),
+      "expireAt" -> "Change this entire line to match the original Mongo document",
+      "lastUpdated" -> "Change this entire line to match the original Mongo document"
+    )
+  }
+
+  def generateEvent8ASummaryJson(eventType: String, numberOfMembers: Int): JsObject = {
+    val arrayOfMembers = for {
+      num <- (1 to numberOfMembers).toList
+    } yield {
+      Json.parse(
+        s"""
+          |{
+          |  "membersDetails": {
+          |    "firstName": "Hello",
+          |    "lastName": "World",
+          |    "nino": "AB${100000 + num}C"
+          |  },
+          |  "paymentType": "paymentOfAStandAloneLumpSum",
+          |  "typeOfProtection": "primaryProtection",
+          |  "typeOfProtectionReference": "12345678",
+          |  "lumpSumAmountAndDate": {
+          |    "lumpSumAmount": 10.01,
+          |    "lumpSumDate": "2022-12-12"
+          |  }
+          |}
+          |""".stripMargin)
+    }
+    Json.obj(
+      "_id" -> "Change this entire line to match the original Mongo document",
+      "apiTypes" -> "1830",
+      "pstr" -> "Change this entire line to match the original Mongo document",
+      "data" -> Json.obj(
+        eventType -> Json.obj(
+          "members" -> arrayOfMembers
+        )
+      ),
+      "expireAt" -> "Change this entire line to match the original Mongo document",
+      "lastUpdated" -> "Change this entire line to match the original Mongo document"
+    )
+  }
+
   def generateEvent22Or23SummaryJson(eventType: String, numberOfMembers: Int, year: Int): JsObject = {
     val arrayOfMembers = for {
-      _ <- (1 to numberOfMembers).toList
+      num <- (1 to numberOfMembers).toList
     } yield {
       Json.obj(
         "membersDetails" -> Json.obj(
           "firstName" -> Name.first_name,
           "lastName" -> Name.last_name,
-          "nino" -> s"AB${new Random().between(100000, 999999)}C"
+          "nino" -> s"AB${100000 + num}C"
         ),
         "chooseTaxYear" -> s"${year}",
         "totalPensionAmounts" -> BigDecimal(new Random().between(1: Float, 1000: Float)).setScale(2, BigDecimal.RoundingMode.HALF_UP)
@@ -128,13 +376,13 @@ object StubDataGenerator extends App {
       "fixedProtection2014", "fixedProtection2016", "individualProtection2014", "individualProtection2016"
     )
     val arrayOfMembers = for {
-      _ <- (1 to numberOfMembers).toList
+      num <- (1 to numberOfMembers).toList
     } yield {
       Json.obj(
         "membersDetails" -> Json.obj(
           "firstName" -> Name.first_name,
           "lastName" -> Name.last_name,
-          "nino" -> s"AB${new Random().between(100000, 999999)}C"
+          "nino" -> s"AB${100000 + num}C"
         ),
         "typeOfProtection" -> s"${new Random().shuffle(protectionTypes).head}",
         "inputProtectionType" ->  s"${new Random().between(10000000, 99999999)}",
@@ -159,25 +407,48 @@ object StubDataGenerator extends App {
   }
 
   // Uncomment below to write files.
-    private val numOfMembers = 18000
+    private val numOfMembers = 1000
     private val taxYear = DateHelper.currentYear - 1
 
 //   generate data for event22
-   writeFileToConfResources("event22", generateEvent22Or23SummaryJson("event22", numOfMembers, taxYear).toString(), s"${numOfMembers.toString}Members${taxYear}Payload.json")
+//   writeFileToConfResources("event22", generateEvent22Or23SummaryJson("event22", numOfMembers, taxYear).toString(), s"${numOfMembers.toString}Members${taxYear}Payload.json")
 
   // generate csv data for event22
   // writeFileToConfResources("event22", generateEvent22SummaryCsv(numOfMembers, taxYear), s"${numOfMembers.toString}Members${taxYear}Payload.csv")
 
   // generate json data for event6
-  //writeFileToConfResources("event6", generateEvent6SummaryJson("event6", numOfMembers, taxYear).toString(), s"${numOfMembers.toString}Members${taxYear}Payload.json")
+  // writeFileToConfResources("event6", generateEvent6SummaryJson("event6", numOfMembers, taxYear).toString(), s"${numOfMembers.toString}Members${taxYear}Payload.json")
 
   // generate csv data for event6
-  //writeFileToConfResources("event6", generateEvent6SummaryCsv(numOfMembers, taxYear), s"${numOfMembers.toString}Members${taxYear}Payload.csv")
+  // writeFileToConfResources("event6", generateEvent6SummaryCsv(numOfMembers, taxYear), s"${numOfMembers.toString}Members${taxYear}Payload.csv")
 
   // generate csv data for event23
   // writeFileToConfResources("event23", generateEvent22Or23SummaryCsv(numOfMembers, taxYear), s"${numOfMembers.toString}Members${taxYear}Payload.csv")
 
   // generate json data for event23
   // writeFileToConfResources("event23", generateEvent22Or23SummaryJson("event23", numOfMembers, taxYear).toString(), s"${numOfMembers.toString}Members${taxYear}Payload.json")
+
+
+  // generate json data for event2
+  // writeFileToConfResources("event2", generateEvent2SummaryJson("event2", numOfMembers).toString(), s"event2_${numOfMembers.toString}Members${taxYear}Payload.json")
+
+  // generate json data for event3
+  //writeFileToConfResources("event3", generateEvent3SummaryJson("event3", numOfMembers).toString(), s"event3_${numOfMembers.toString}Members${taxYear}Payload.json")
+
+  // generate json data for event4
+  // writeFileToConfResources("event4", generateEvent4SummaryJson("event4", numOfMembers).toString(), s"event4_${numOfMembers.toString}Members${taxYear}Payload.json")
+
+  // generate json data for event5
+  // writeFileToConfResources("event5", generateEvent5SummaryJson("event5", numOfMembers).toString(), s"event5_${numOfMembers.toString}Members${taxYear}Payload.json")
+
+  // generate json data for event7
+  // writeFileToConfResources("event7", generateEvent7SummaryJson("event7", numOfMembers).toString(), s"event7_${numOfMembers.toString}Members${taxYear}Payload.json")
+
+  // generate json data for event8
+  //writeFileToConfResources("event8", generateEvent8SummaryJson("event8", numOfMembers).toString(), s"event8_${numOfMembers.toString}Members${taxYear}Payload.json")
+
+
+  // generate json data for event8A
+  // writeFileToConfResources("event8A", generateEvent8ASummaryJson("event8A", numOfMembers).toString(), s"event8A_${numOfMembers.toString}Members${taxYear}Payload.json")
 
 }
